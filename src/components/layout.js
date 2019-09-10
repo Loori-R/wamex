d505d9d3f91eed9adc960a1f2180971d7c5e30c7
Yury <yurymolodov@gmail.com>
2019-09-11 00:47:18 +0300

    feat: add title block

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header/header"
+import TopTitle from "./TopTitle/topTitle"
 import "./layout.css"
+
+const topTitleText =
+  "MAYR ANTRIEBSTECHNIK - мировой лидер в разработке и производстве предохранительных муфт и тормозов"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,6 +23,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <TopTitle title={topTitleText} />
       <div>
         <main>{children}</main>
         <footer>
