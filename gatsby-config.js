29bf37674dde05cf2dd265c41961339b68547bbe
Yury <yurymolodov@gmail.com>
2019-09-16 23:22:49 +0300

    fix: fix start_url

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/wamex/`,
+        name: `WAMEX - Современная беззазорная приводная техника`,
+        short_name: `Wamex`,
+        start_url: `.`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
